b14fe5140bb79ba98ae34cb2da16cf25ce3aea54
Dario de Oliveira Junior <dariooljr@gmail.com>
2021-04-02 08:58:47 -0300

    Create Rentals migration

--- src/shared/infra/typeorm/migrations/1616672832342-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1616672832342-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1616672832342 implements MigrationInterface {
           {
             name: "id",
             type: "uuid",
+            isPrimary: true,
           },
           {
             name: "name",
