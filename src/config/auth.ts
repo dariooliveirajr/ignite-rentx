6ccaebbba1792b12874ef8c92d14a2a74b166044
Dario de Oliveira Junior <dariooljr@gmail.com>
2021-04-19 20:43:01 -0300

    Refactor user Auth, add refresh token

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: "57f3b146dbe9f2550c9b775500525266",
+  expires_in_token: "15m",
+  secret_refresh_token: "58d89034f2460c762983170e308cf748",
+  expires_in_refresh_token: "30d",
+  expires_refresh_token_days: 30,
+};
